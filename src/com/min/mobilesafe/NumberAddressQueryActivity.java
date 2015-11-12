756217c37c63c9d93802ff3c087eeba64d2e0e38
spiritpig <tceh528@126.com>
2015-11-12 21:10:34 +0800

    分类查询号码，添加抖动动画

--- src/com/min/mobilesafe/NumberAddressQueryActivity.java
+++ src/com/min/mobilesafe/NumberAddressQueryActivity.java
@@ -4,15 +4,20 @@ import com.min.mobilesafe.db.dao.NumberAddressQueryUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class NumberAddressQueryActivity extends Activity {
 	
+	private String databasePath = null;	//这个是数据库文件的路径,初始话不能写在这个后面,getFilesDir()会包空指针错误
 	private EditText etPhone = null;
 	private TextView tvResult = null;
 
@@ -20,17 +25,38 @@ public class NumberAddressQueryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_number_address_query);
+		databasePath = getFilesDir().getAbsolutePath() + "/address.db";
 		
 		etPhone = (EditText) findViewById(R.id.et_phone);
 		tvResult = (TextView) findViewById(R.id.tv_result);
+		
+		etPhone.addTextChangedListener(new TextWatcher() {
+			
+			//输入框发生了变化
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if (null != s && s.length() >= 3) {
+					tvResult.setText(NumberAddressQueryUtils.queryNumber(s.toString(), databasePath));
+				}
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {}
+			
+			@Override
+			public void afterTextChanged(Editable s) {}
+		});
 	}
 	
 	public void query(View view) {
 		String phone = etPhone.getText().toString();
 		if (TextUtils.isEmpty(phone)) {
+			Animation anim = AnimationUtils.loadAnimation(this, R.anim.shake);//抖动动画
+			etPhone.startAnimation(anim);
 			Toast.makeText(this, "请输入要查询的号码", Toast.LENGTH_LONG).show();
 		} else {
-			tvResult.setText(NumberAddressQueryUtils.queryNumber(phone, getFilesDir().getAbsolutePath() + "/address.db"));
+			tvResult.setText(NumberAddressQueryUtils.queryNumber(phone, databasePath));
 			Log.i("--->", "--->查询" + phone);
 		}
 	}
--- src/com/min/mobilesafe/db/dao/NumberAddressQueryUtils.java
+++ src/com/min/mobilesafe/db/dao/NumberAddressQueryUtils.java
@@ -18,15 +18,53 @@ public class NumberAddressQueryUtils {
 	 */
 	public static String queryNumber(String number, String databasePath) {
 		String address = number;
-		
 		SQLiteDatabase database = SQLiteDatabase.openDatabase(databasePath, null, SQLiteDatabase.OPEN_READONLY);
-		Cursor cursor = database.rawQuery(
-				"select location from data2 where id = (select outkey from data1 where id = ?)",
-				new String[]{number.substring(0, 7)});
-		while (cursor.moveToNext()) {
-			address = cursor.getString(0);
+		if (number.matches("1[34568]\\d{9}$")) {
+			Cursor cursor = database.rawQuery(
+					"select location from data2 where id = (select outkey from data1 where id = ?)",
+					new String[]{number.substring(0, 7)});
+			while (cursor.moveToNext()) {
+				address = cursor.getString(0);
+			}
+			cursor.close();
+		} else {
+			switch (number.length()) {
+			case 3:
+				address = "报警电话";
+				break;
+			case 4:
+				address = "模拟器";		
+				break;
+			case 5:
+				address = "客服电话";
+				break;
+			case 7:
+			case 8:
+				address = "本地号码";
+				break;
+			default:
+				if (number.length() > 10 && number.startsWith("0")) {//长途电话,010-7788120
+					Cursor cursor = database.rawQuery(
+							"select location from data2 where area = ?",
+							new String[]{number.substring(1, 3)});//3位区号
+					while (cursor.moveToNext()) {
+						address = cursor.getString(0);
+						address = address.substring(0, address.length() - 2);	//只需要地名，不要运营商名
+					}
+					cursor.close();
+					
+					cursor = database.rawQuery(
+							"select location from data2 where area = ?",
+							new String[]{number.substring(1, 4)});	//4位区号
+					while (cursor.moveToNext()) {
+						address = cursor.getString(0);
+						address = address.substring(0, address.length() - 2);	//只需要地名，不要运营商名
+					}
+					cursor.close();
+				}
+				break;
+			}
 		}
-		cursor.close();
 		
 		return address;
 	}
