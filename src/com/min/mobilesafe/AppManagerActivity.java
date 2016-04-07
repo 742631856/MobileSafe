bfe1d0a9c008b142ebe4224fdcefd7d979a789a8
742631856 <742631856@qq.com>
2016-04-07 22:41:03 +0800

    软件的卸载、启动、分享实现

--- src/com/min/mobilesafe/AppManagerActivity.java
+++ src/com/min/mobilesafe/AppManagerActivity.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StatFs;
 import android.text.format.Formatter;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -26,17 +31,19 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.min.mobilesafe.domain.AppInfo;
 import com.min.mobilesafe.engine.AppInfoProvider;
 import com.min.mobilesafe.utils.DensityUtil;
 
-public class AppManagerActivity extends Activity {
+public class AppManagerActivity extends Activity implements OnClickListener {
 
 	private TextView tvRom;
 	private TextView tvSDCard;
 	private View ll_loading;
 	private ListView lvAppInfo;
+	private AppAdapter appAdapter;
 	
 	private List<AppInfo> userAppInfos;
 	private List<AppInfo> sysAppInfos;
@@ -51,11 +58,14 @@ public class AppManagerActivity extends Activity {
 //	private TextView tvPop = null;
 	private PopupWindow popWin;
 	
+	private PackageManager pm;
+	private AppInfo appinfo;	//选中行的包名
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_app_manager);
+		pm = getPackageManager();
 		
 		ll_loading = findViewById(R.id.ll_loading);
 		ll_loading.setVisibility(View.VISIBLE);
@@ -101,14 +111,13 @@ public class AppManagerActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				
-				/*AppInfo appInfo = null;
 				if (0 == position || sysAppInfos.size() + 1 == position) {
 					return;
 				} else if (position <= sysAppInfos.size()) {
-					appInfo = sysAppInfos.get(position - 1);
+					appinfo = sysAppInfos.get(position - 1);
 				} else {
-					appInfo = userAppInfos.get(position - 1 - sysAppInfos.size() - 1);
-				}*/
+					appinfo = userAppInfos.get(position - 1 - sysAppInfos.size() - 1);
+				}
 				dismissPopWindow();	//去掉以前的
 				
 //				tvPop.setText(appInfo.packName);
@@ -135,10 +144,17 @@ public class AppManagerActivity extends Activity {
 		});
 	}
 	
+	/**
+	 * 去掉弹出框
+	 */
 	private void dismissPopWindow() {
 		if (null == popWin) {
 			//这个作为PopupWindow的ContentView
 			View view = View.inflate(this, R.layout.popup_app_item, null);
+			//给三个按钮添加点击事件
+			view.findViewById(R.id.ll_uninstall).setOnClickListener(this);
+			view.findViewById(R.id.ll_launch).setOnClickListener(this);
+			view.findViewById(R.id.ll_share).setOnClickListener(this);
 			//弹出窗口，包裹类容容
 			popWin = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 			// PopupWindow 是没有背景的，android中没有背景的窗口是不能播放动画效果的，所以加上一个背景
@@ -147,7 +163,107 @@ public class AppManagerActivity extends Activity {
 		popWin.dismiss();
 	}
 	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.ll_uninstall:
+				uninstallApplication();
+				break;
+			case R.id.ll_launch:
+				launchApplication();
+				break;
+			case R.id.ll_share:
+				shareApplication();
+				break;
+	
+			default:
+				break;
+		}
+		dismissPopWindow();
+	}
+	
+	/**
+	 * 启动app
+	 */
+	private void launchApplication() {
+		// 下面是查询所有可以启动的app
+//		Intent intent = new Intent(Intent.ACTION_MAIN);
+//		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+//		//查询所有可以启动的 Activities
+//		List<ResolveInfo> launcherActivities = pm.queryIntentActivities(intent, PackageManager.GET_INTENT_FILTERS);
+		
+		// 获取包名的启动意图
+		Intent intent = pm.getLaunchIntentForPackage(appinfo.packName);
+		if (null != intent) {
+			startActivity(intent);
+		} else {	// 若意图为空，则说明该应用无法启动
+			Toast.makeText(this, "无法启动该应用", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	/**
+	 * 卸载app
+	 */
+	private void uninstallApplication() {
+		/*
+		 * <intent-filter>
+                <action android:name="android.intent.action.VIEW" />
+                <action android:name="android.intent.action.DELETE" />
+                <category android:name="android.intent.category.DEFAULT" />
+                <data android:scheme="package" />
+            </intent-filter>
+		 * */
+		if (appinfo.userApp) {
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.setAction(Intent.ACTION_DELETE);
+			intent.addCategory(Intent.CATEGORY_DEFAULT);
+			intent.setData(Uri.parse("package:" + appinfo.packName));
+			startActivityForResult(intent, 11);
+		} else {
+			Toast.makeText(this, "无法卸载系统软件", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	/**
+	 * 分享app
+	 */
+	private void shareApplication() {
+		/*
+		 <intent-filter>
+               <action android:name="android.intent.action.SEND" />
+               <category android:name="android.intent.category.DEFAULT" />
+               <data android:mimeType="text/plain" />
+           </intent-filter> 
+		 */
+		
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.addCategory(Intent.CATEGORY_DEFAULT);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT, "给你介绍一个好用的软件: " + appinfo.name);
+		startActivity(intent);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		Log.i("--->", "--->" + resultCode);
+		if (11 == requestCode) {// 卸载请求，并且卸载成功
+			getAppInfos();
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	/**
+	 * 获取所有的应用包名
+	 */
 	private void getAppInfos() {
+		if (null != userAppInfos) {
+			userAppInfos.clear();
+			userAppInfos = null;
+		}
+		if (null != sysAppInfos) {
+			sysAppInfos.clear();
+			sysAppInfos = null;
+		}
 		new Thread() {
 			public void run() {
 				List<AppInfo> appInfos = AppInfoProvider.getAppInfos(AppManagerActivity.this);
@@ -166,7 +282,13 @@ public class AppManagerActivity extends Activity {
 				
 				runOnUiThread(new Runnable() {
 					public void run() {
-						lvAppInfo.setAdapter(new AppAdapter());
+						if (null == appAdapter) {
+							appAdapter = new AppAdapter();
+							lvAppInfo.setAdapter(appAdapter);
+						} else {
+							appAdapter.notifyDataSetChanged();
+						}
+						
 						ll_loading.setVisibility(View.INVISIBLE);
 					}
 				});
@@ -174,6 +296,11 @@ public class AppManagerActivity extends Activity {
 		}.start();
 	}
 	
+	/**
+	 * 获取可用空间
+	 * @param path 路径
+	 * @return
+	 */
 	private long getAviableSpace(String path) {
 		StatFs statfs = new StatFs(path);
 		long blocks = statfs.getAvailableBlocks();
